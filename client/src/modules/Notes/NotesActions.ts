5a8c1a27b3decd98bc3c57448c57d68d0f0df77c
System Administrator <root@admins-MacBook-Pro.local>
2018-07-22 22:25:41 +0300

    Unsucessful perfect post note api

--- client/src/modules/Notes/NotesActions.ts
+++ client/src/modules/Notes/NotesActions.ts
@@ -3,10 +3,14 @@ import { RootState } from "../../app/state.interface";
 
 export const FETCH_NOTES_INIT_DATA_START = "FETCH_NOTES_INIT_DATA_START";
 export const FETCH_NOTES_INIT_DATA_SUCCESS = "FETCH_NOTES_INIT_DATA_SUCCESS";
-const QUOTE_SERVICE_URL = "http://localhost:3000/api/notes";
+
+export const START_NOTES_REQUEST = "START_NOTES_REQUEST";
+export const SUCCESS_NOTES_REQUEST = "SUCCESS_NOTES_REQUEST";
+export const FAILED_NOTES_REQUEST = "FAILED_NOTES_REQUEST";
+const NOTE_SERVICE_URL = "http://localhost:3000/api/notes";
 
 async function getNotes() {
-  const response = await fetch(QUOTE_SERVICE_URL);
+  const response = await fetch(NOTE_SERVICE_URL);
   return await response.json();
 }
 
@@ -27,3 +31,53 @@ export const fetchNotesInitDataSuccess = notes => ({
   type: FETCH_NOTES_INIT_DATA_SUCCESS,
   payload: notes
 });
+
+export const setNotesStart = () => ({
+  type: START_NOTES_REQUEST,
+  payload: undefined,
+});
+
+export const setNotesSuccess = () => ({
+  type: SUCCESS_NOTES_REQUEST,
+  payload: undefined,
+});
+
+export const setNotesFailed = () => ({
+  type: FAILED_NOTES_REQUEST,
+  payload: undefined,
+});
+
+export const sendNotesToApi = async (notes): Promise<void> => {
+  const apiResponse = await fetch(NOTE_SERVICE_URL, {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        title: 'test',
+        description: 'testing'
+      }
+    ),
+  });
+  if (apiResponse.status !== 204) {
+    throw new Error(`Request failed: non-204 response from API: [${apiResponse.status}]`);
+  }
+};
+
+
+export function createSubmitNotesAction(notes) {
+  console.log(notes);
+  const test = {
+    title: 'test',
+    description: 'testing'
+  }
+  return async (dispatch: Dispatch<RootState>, getState: () => RootState) => {
+    try {
+      dispatch(setNotesStart());
+      await sendNotesToApi(test);
+      dispatch(setNotesSuccess());
+    } catch (err) {
+      // tslint:disable-next-line:no-console
+      console.error(err);
+      dispatch(setNotesFailed());
+    }
+  };
+}
--- client/src/modules/Notes/index.tsx
+++ client/src/modules/Notes/index.tsx
@@ -2,27 +2,47 @@ import * as React from "react";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
 import { RootState } from "../../app/state.interface";
-import { createFetchDataFromBackendAction } from "./NotesActions";
+import { createFetchDataFromBackendAction, createSubmitNotesAction } from "./NotesActions";
+import { PrimaryButton } from "../../common/buttons";
 
 export type NotesProps = {
   fetchNotesWithFetch: () => void;
   notes: any;
+  handleSubmitNewNote: (notes) => void;
 };
 
 export type ComponentState = {
   isFetching: boolean;
+  inputValue: string;
 };
 
 export class NotesScreen extends React.Component<NotesProps, ComponentState> {
   constructor(props: NotesProps) {
     super(props);
-    this.state = { isFetching: false };
+    this.state = { isFetching: false, inputValue: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+
 
   componentDidMount() {
     this.props.fetchNotesWithFetch();
   }
 
+  handleSubmit(event) {
+    console.log('Form value: ' + this.state.inputValue);
+    event.preventDefault();
+    this.props.handleSubmitNewNote(this.state.inputValue);
+  }
+
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    })
+
+  }
   render() {
     if (!this.props.notes) {
       return;
@@ -36,6 +56,10 @@ export class NotesScreen extends React.Component<NotesProps, ComponentState> {
             </li>
           ))}
         </ul>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+          <PrimaryButton type="submit" value="Submit">Add new</PrimaryButton>
+        </form>
       </div>
     );
   }
@@ -50,7 +74,10 @@ export const notesScreenDispatchToProps = (dispatch: Dispatch<RootState>) => {
   return {
     fetchNotesWithFetch: () => {
       dispatch(createFetchDataFromBackendAction());
-    }
+    },
+    handleSubmitNewNote: (notes) => {
+      dispatch(createSubmitNotesAction(notes));
+    },
   };
 };
 
