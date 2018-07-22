810283ec9e9fc3844280e5a0ad322bf4addb4824
System Administrator <root@admins-MacBook-Pro.local>
2018-07-22 18:40:03 +0300

    Remove redundant HomeScreen render

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -31,7 +31,6 @@ class App extends React.Component {
               )}
             />
           </Router>
-          <HomeScreen />
         </AppWrapper>
       </Provider>
     );
--- client/src/modules/HomeScreen/index.tsx
+++ client/src/modules/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-const HomeScreen = () => <p>Loading ...</p>;
+const HomeScreen = () => <p>This is an e2e sls, express, mongodb and react app.</p>;
 
 export default HomeScreen;
