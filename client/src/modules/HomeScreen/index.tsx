72c4656fbda89ba45204875d74e638fb63096c54
System Administrator <root@admins-MacBook-Pro.local>
2018-07-22 18:40:24 +0300

    Lint fixes

--- client/src/modules/HomeScreen/index.tsx
+++ client/src/modules/HomeScreen/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
-const HomeScreen = () => <p>This is an e2e sls, express, mongodb and react app.</p>;
+const HomeScreen = () => (
+  <p>This is an e2e sls, express, mongodb and react app.</p>
+);
 
 export default HomeScreen;
