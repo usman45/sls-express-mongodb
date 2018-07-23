90281b28533be80701e0f5dc1347f1fb06bdf6e1
usman45 <muhammad.usman@finnair.com>
2018-07-23 09:38:41 +0300

    Some prettier lint left behind

--- client/src/modules/Notes/index.tsx
+++ client/src/modules/Notes/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
-import {Dispatch} from "redux";
-import {connect} from "react-redux";
-import {RootState} from "../../app/state.interface";
-import {createFetchDataFromBackendAction, createSubmitNotesAction} from "./NotesActions";
-import {PrimaryButton} from "../../common/buttons";
+import { Dispatch } from "redux";
+import { connect } from "react-redux";
+import { RootState } from "../../app/state.interface";
+import {
+  createFetchDataFromBackendAction,
+  createSubmitNotesAction
+} from "./NotesActions";
+import { PrimaryButton } from "../../common/buttons";
 
 export type NotesProps = {
   fetchNotesWithFetch: () => void;
@@ -92,7 +95,6 @@ export const notesScreenDispatchToProps = (dispatch: Dispatch<RootState>) => {
   };
 };
 
-export default connect(
-  notesStateToProps,
-  notesScreenDispatchToProps
-)(NotesScreen);
+export default connect(notesStateToProps, notesScreenDispatchToProps)(
+  NotesScreen
+);
